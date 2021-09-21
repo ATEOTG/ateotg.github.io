f3598c8c698dcdb8125658dfc833a6d11a869c84
ATEOTG <bryansantos9797@gmail.com>
2021-09-20 23:42:26 -0400

    final commit, I will become a React pro!

--- src/App.js
+++ src/App.js
@@ -7,16 +7,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          
           My name is Bryan De Los Santos and I'm going to master React!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/ATEOTG"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check Me Out!
         </a>
       </header>
     </div>
