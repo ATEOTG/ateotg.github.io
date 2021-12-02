8f4970cafdc00ab8e695327a4d22954fcfc38ab8
ATEOTG <bryansantos9797@gmail.com>
2021-12-01 19:06:07 -0500

    added components folder

--- src/App.js
+++ src/App.js
@@ -4,21 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+      
+      
+      <img src={logo} className="App-logo" alt="logo" />
+        <h1>
           
           My name is Bryan De Los Santos and I'm going to master React!
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/ATEOTG"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Check Me Out!
-        </a>
-      </header>
+        </h1>
+
+      
     </div>
   );
 }
