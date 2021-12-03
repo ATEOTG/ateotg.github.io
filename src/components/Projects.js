0b99eb9e5c92b2eb2a643ae0c87e27f9bf08803b
ATEOTG <bryansantos9797@gmail.com>
2021-12-02 21:49:20 -0500

    added Project page nav and individual project page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,12 +1,22 @@
 import React from "react";
+import {Routes, Route, Link} from "react-router-dom";
+import Project1 from "../components/Project1.js";
 
 function Projects() {
+
+   
+    
     return(
-        <>
+        <div>
         
-            <h2> This is the Projects page</h2>
-  
-        </>
+            <h1> My Projects: </h1>
+
+            <nav>
+                <Link to ="Project1"> <h2>Project 1</h2> </Link>
+            </nav>
+        
+       
+        </div>
 
     );
 }
